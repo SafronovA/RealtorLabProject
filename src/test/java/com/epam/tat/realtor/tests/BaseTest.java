3e3aac2f96bf74b0b15c620bd0be5491fa790629
Siarhei Volchak <Siarhei_Volchak@epam.com>
2018-10-02 14:50:12 +0300

    update

--- src/test/java/com/epam/tat/realtor/tests/BaseTest.java
+++ src/test/java/com/epam/tat/realtor/tests/BaseTest.java
@@ -1,5 +1,6 @@
 
- package com.epam.tat.realtor.tests;
+package com.epam.tat.realtor.tests;
+
 import com.epam.tat.realtor.ConfigProperties;
 import com.epam.tat.realtor.drivers.DriverFactory;
 import com.epam.tat.realtor.steps.HomePageStep;
@@ -7,11 +8,13 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.*;
 import org.openqa.selenium.NoSuchElementException;
+
 import java.util.concurrent.TimeUnit;
 
 public class BaseTest {
     protected WebDriver driver;
     protected HomePageStep homePageStep;
+
     /**
      * init specified WebDriver
      * configure driver implicitlyWaite timeouts
@@ -19,36 +22,34 @@ public class BaseTest {
      * open the homepage URL in browser
      */
     @BeforeClass(alwaysRun = true)
-    void initPage(){
+    void initPage() {
         driver = DriverFactory.CHROMEDRIVER.getDriver();
         driver.manage().deleteAllCookies();
         driver.manage().window().maximize();
         driver.navigate().to(ConfigProperties.getTestProperty("url"));
         homePageStep = new HomePageStep(driver);
         driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-        boolean isNewPage=true;
+        boolean isNewPage = true;
         while (isNewPage) {
             try {
-                isNewPage=false;
+                isNewPage = false;
                 driver.findElement(By.xpath("//*[@id='searchBox']"));
             } catch (NoSuchElementException e) {
                 System.out.println("New version of the home page. Page has to be  reloaded...");
-                isNewPage=true;
+                isNewPage = true;
                 driver.manage().deleteAllCookies();
                 driver.navigate().to(ConfigProperties.getTestProperty("url"));
             } finally {
                 driver.manage().timeouts().implicitlyWait(Integer.valueOf(ConfigProperties.getTestProperty("implicitlyWaitTime")), TimeUnit.SECONDS);
-
             }
         }
-
-
     }
+
     /**
      * close browser
      */
-    @AfterTest
-    void closeResources(){
+    @AfterClass
+    void closeResources() {
         driver.quit();
     }
 }
