12b30faa921247eb7b84fd72051c250a6c55cfab
Siarhei Volchak <Siarhei_Volchak@epam.com>
2018-10-03 12:56:55 +0300

    update realtorPage javadoc

--- src/main/java/com/epam/tat/realtor/pages/RealtorPage.java
+++ src/main/java/com/epam/tat/realtor/pages/RealtorPage.java
@@ -8,11 +8,12 @@ import org.openqa.selenium.support.PageFactory;
 
 import java.util.List;
 
-public class RealtorPage extends BasePage{
-    public RealtorPage(WebDriver driver){
+public class RealtorPage extends BasePage {
+    public RealtorPage(WebDriver driver) {
         super(driver);
-        PageFactory.initElements(driver,this);
+        PageFactory.initElements(driver, this);
     }
+
     By soldHouses = By.xpath("//div[@class='leaflet-marker-pane']/div");
     @FindBy(xpath = "//ul[@class='nav nav-tabs nav-pills']//a[contains(text(),'Recently Sold')]")
     private WebElement soldHousesSection;
@@ -26,39 +27,40 @@ public class RealtorPage extends BasePage{
     private WebElement askQuestionButton;
     @FindBy(xpath = "//a[@class='btn btn-plain load-more-button load-more-review-button']")
     private WebElement loadMoreReviewsButton;
-    @FindBy (xpath = "//ul[@id='agent-review-list']/li[not(@style='display: none;')]")
+    @FindBy(xpath = "//ul[@id='agent-review-list']/li[not(@style='display: none;')]")
     private List<WebElement> realtorReviews;
 
 
     /**
      * get realtor reviews list
-     * @return  realtor reviews
+     *
+     * @return realtor reviews
      */
-    public List<WebElement> getRealtorReviews(){
+    public List<WebElement> getRealtorReviews() {
         return realtorReviews;
     }
 
     /**
-     *
      * @return list of sold houses map marks
      */
-    public List<WebElement> getSoldHousesMapMarkList(){
+    public List<WebElement> getSoldHousesMapMarkList() {
         return soldHousesMapMarkList;
     }
 
     /**
-     *
      * @return sold status of the house on the map mark
      */
-    public String getSaleHouseStatus(){
+    public String getSaleHouseStatus() {
         waitUntilElementIsVisible(saleHouseStatus);
         return saleHouseStatus.getText();
     }
+
     /**
      * click "Recently Sold" section above iframe map
+     *
      * @return this page
      */
-    public RealtorPage clickSoldHousesSection(){
+    public RealtorPage clickSoldHousesSection() {
         waitUntilElementIsClickable(soldHousesSection);
         soldHousesSection.click();
         return this;
@@ -66,19 +68,21 @@ public class RealtorPage extends BasePage{
 
     /**
      * scroll to iframe map
+     *
      * @return this page
      */
-    public RealtorPage scrollToIFrame(){
+    public RealtorPage scrollToIFrame() {
         waitForPresenceOfAllElementsLocatedBy(soldHouses);
-        BasePage.scrollToElement(askQuestionButton,driver);
+        BasePage.scrollToElement(askQuestionButton, driver);
         return this;
     }
 
     /**
      * double click on zoom out button
+     *
      * @return this page
      */
-    public RealtorPage doubleZoomOut(){
+    public RealtorPage doubleZoomOut() {
         waitUntilElementIsVisible(zoomOutButton);
         zoomOutButton.click();
         zoomOutButton.click();
@@ -87,18 +91,20 @@ public class RealtorPage extends BasePage{
 
     /**
      * wait for the presence of sold houses map marks on iframe map
+     *
      * @return this page
      */
-    public RealtorPage waitForMapMarks(){
+    public RealtorPage waitForMapMarks() {
         waitForPresenceOfAllElementsLocatedBy(soldHouses);
         return this;
     }
 
     /**
      * click Load More Reviews button
+     *
      * @return this page
      */
-    public RealtorPage clickLoadMoreReviewsButton(){
+    public RealtorPage clickLoadMoreReviewsButton() {
         waitUntilElementIsClickable(loadMoreReviewsButton);
         loadMoreReviewsButton.click();
         return this;
@@ -106,9 +112,10 @@ public class RealtorPage extends BasePage{
 
     /**
      * check if LoadMoreReviews button is displayed
+     *
      * @return if LoadMoreReviews button is displayed
      */
-    public boolean isLoadMoreReviewsButtonDisplayed(){
+    public boolean isLoadMoreReviewsButtonDisplayed() {
         return loadMoreReviewsButton.isDisplayed();
     }
 }
