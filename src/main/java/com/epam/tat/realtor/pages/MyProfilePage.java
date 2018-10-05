3689d6b4cacc34dc50e36ae029e8e1a8fdfc0aa8
Aliaksei Safronau <Aliaksei_Safronau@epam.com>
2018-10-05 11:30:59 +0300

    add test code

--- src/main/java/com/epam/tat/realtor/pages/MyProfilePage.java
+++ src/main/java/com/epam/tat/realtor/pages/MyProfilePage.java
@@ -6,6 +6,8 @@ import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
+import java.util.List;
+
 public class MyProfilePage extends BasePage {
 
     public MyProfilePage(WebDriver driver) {
@@ -27,6 +29,20 @@ public class MyProfilePage extends BasePage {
     private WebElement saveChangesButton;
     @FindBy(xpath = "//*[@id='__next']//h2")
     private WebElement profileName;
+    @FindBy(xpath = "//*[@class='address-line'][1]")
+    private WebElement profileAddress;
+    @FindBy(xpath = "//*[@class='address-line'][2]")
+    private WebElement profileCityAndState;
+    @FindBy(xpath = "//*[@class='address-line'][3]")
+    private WebElement profileCountry;
+    @FindBy(id = "editAddress1")
+    private WebElement addressInput;
+    @FindBy(id = "editCity")
+    private WebElement cityInput;
+    @FindBy(xpath = "//*[@id='editState']/option")
+    private List<WebElement> stateSelect;
+    @FindBy(xpath = "//*[@id='editCountry']/option")
+    private List<WebElement> countrySelect;
 
     /**
      * @return current profile name
@@ -44,36 +60,47 @@ public class MyProfilePage extends BasePage {
     }
 
     /**
-     * navigate to user icon
-     *
-     * @return this page
+     * @return current profile address
      */
-    public MyProfilePage navigateToUserIcon() {
-        new Actions(driver).moveToElement(userIcon).perform();
-        return this;
+    public String getProfileAddress() {
+        String address = profileAddress.getAttribute("innerHTML");
+        return address;
     }
 
     /**
-     * click log out link in drop-down list, which appears after hovering the cursor on the user's logo
-     *
-     * @return new HomePage
+     * @return current profile cityAndState
      */
-    public HomePage clickLogOutLink() {
-        waitUntilElementIsClickable(logOutLink);
-        logOutLink.click();
-        return new HomePage(driver);
+    public String getProfileCityAndState() {
+        String cityAndState = profileCityAndState.getAttribute("innerHTML");
+        return cityAndState;
     }
 
     /**
-     * click 'Edit Profile' button
-     *
-     * @return this page
+     * @return current profile country
      */
-    public MyProfilePage clickEditProfileButton() {
-        waitUntilElementIsVisible(editProfileButton);
-        waitUntilElementIsClickable(editProfileButton);
-        editProfileButton.click();
-        return this;
+    public String getProfileCountry() {
+        String country = profileCountry.getAttribute("innerHTML");
+        return country;
+    }
+    /**
+     * @return WebElement (@code profileAddress)
+     */
+    public WebElement getProfileAddressWebElement() {
+        return profileAddress;
+    }
+
+    /**
+     * @return WebElement (@code profileCityAndState)
+     */
+    public WebElement getProfileCityAndStateWebElement() {
+        return profileCityAndState;
+    }
+
+    /**
+     * @return WebElement (@code profileCountry)
+     */
+    public WebElement getProfileCountryWebElement() {
+        return profileCountry;
     }
 
     /**
@@ -101,12 +128,91 @@ public class MyProfilePage extends BasePage {
     }
 
     /**
+     * enter address in (@code addressInput)
+     *
+     * @param address required address
+     * @return this page
+     */
+    public MyProfilePage enterAddress(String address) {
+        addressInput.clear();
+        addressInput.sendKeys(address);
+        return this;
+    }
+
+    /**
+     * enter city in (@code cityInput)
+     *
+     * @param city required city
+     * @return this page
+     */
+    public MyProfilePage enterCity(String city) {
+        cityInput.clear();
+        cityInput.sendKeys(city);
+        return this;
+    }
+
+    /**
+     * select state in (@code stateSelect)
+     *
+     * @param state required state
+     * @return this page
+     */
+    public MyProfilePage enterState(String state) {
+        stateSelect.stream().filter(webElement -> webElement.getAttribute("innerHTML").equals(state)).forEach(WebElement::click);
+        return this;
+    }
+
+    /**
+     * select country in (@code countrySelect)
+     *
+     * @param country required country
+     * @return this page
+     */
+    public MyProfilePage enterCountry(String country) {
+        countrySelect.stream().filter(webElement -> webElement.getAttribute("innerHTML").equals(country)).forEach(WebElement::click);
+        return this;
+    }
+
+    /**
      * click 'Save Changes' button
      *
      * @return this page
      */
     public MyProfilePage clickSaveChangesButton() {
         saveChangesButton.click();
+        return this;
+    }
+
+    /**
+     * click log out link in drop-down list, which appears after hovering the cursor on the user's logo
+     *
+     * @return new HomePage
+     */
+    public HomePage clickLogOutLink() {
+        waitUntilElementIsClickable(logOutLink);
+        logOutLink.click();
+        return new HomePage(driver);
+    }
+
+    /**
+     * click 'Edit Profile' button
+     *
+     * @return this page
+     */
+    public MyProfilePage clickEditProfileButton() {
+        waitUntilElementIsVisible(editProfileButton);
+        waitUntilElementIsClickable(editProfileButton);
+        editProfileButton.click();
+        return this;
+    }
+
+    /**
+     * navigate to user icon
+     *
+     * @return this page
+     */
+    public MyProfilePage navigateToUserIcon() {
+        new Actions(driver).moveToElement(userIcon).perform();
         return this;
     }
 }
