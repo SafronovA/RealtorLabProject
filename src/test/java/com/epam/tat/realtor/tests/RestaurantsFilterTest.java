d8cdd030b540b6d59af55b82ab24fc15aafe9d6e
Anastasiya Khapayeva <Anastasiya_Khapayeva@epam.com>
2018-10-08 11:08:27 +0300

    delete excess code

--- src/test/java/com/epam/tat/realtor/tests/RestaurantsFilterTest.java
+++ src/test/java/com/epam/tat/realtor/tests/RestaurantsFilterTest.java
@@ -1,14 +1,8 @@
 package com.epam.tat.realtor.tests;
 
-import com.epam.tat.realtor.pages.BasePage;
 import com.epam.tat.realtor.steps.SearchPageStep;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.Actions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
-
-import java.util.List;
 
 public class RestaurantsFilterTest extends BaseTest {
 
@@ -24,12 +18,6 @@ public class RestaurantsFilterTest extends BaseTest {
                 .clickViewMapButton()
                 .clickLifestyleButton()
                 .selectRestaurants();
-//        List<WebElement> element = driver.findElements(By.xpath("//div[contains(@class,'pin-restaurants')]"));
-//        for (int i = 1; i <element.size()+1 ; i++) {
-//            BasePage.clickByJEx(driver.findElement(By.xpath("//div[contains(@class,'pin-restaurants')]["+i+"]")), driver);
-//
-//        }
-
         Assert.assertTrue(searchPageStep.areAllFoundLifestyleRestaurants(),
                 "One of found lifestyle is not a restaurant");
     }
