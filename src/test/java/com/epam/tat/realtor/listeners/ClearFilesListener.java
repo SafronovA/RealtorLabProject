8c8ad20f0a3dfa0555cd1134c8e2ec7190587e96
Siarhei Volchak <Siarhei_Volchak@epam.com>
2018-10-11 16:26:10 +0300

    1

---
+++ src/test/java/com/epam/tat/realtor/listeners/ClearFilesListener.java
@@ -0,0 +1,16 @@
+package com.epam.tat.realtor.listeners;
+
+import org.testng.ISuite;
+import org.testng.ISuiteListener;
+
+public class ClearFilesListener implements ISuiteListener {
+    @Override
+    public void onStart(ISuite iSuite) {
+
+    }
+
+    @Override
+    public void onFinish(ISuite iSuite) {
+
+    }
+}
