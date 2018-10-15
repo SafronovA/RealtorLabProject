a61522a4d705bf3172909552d2de5115bb27363b
Anastasiya Khapayeva <Anastasiya_Khapayeva@epam.com>
2018-10-15 12:43:16 +0300

    add listener

--- src/test/java/com/epam/tat/realtor/listener/FailedTestListener.java
+++ src/test/java/com/epam/tat/realtor/listener/FailedTestListener.java
@@ -8,27 +8,27 @@ public class FailedTestListener implements ITestListener {
 
     @Override
     public void onTestStart(ITestResult iTestResult) {
-
+        System.out.println("Test " + iTestResult.getTestName() + " start");
     }
 
     @Override
     public void onTestSuccess(ITestResult iTestResult) {
-        
+        System.out.println("Test " + iTestResult.getTestName() + " success");
     }
 
     @Override
     public void onTestFailure(ITestResult iTestResult) {
-
+        System.out.println("Test " + iTestResult.getTestName() + " failed");
     }
 
     @Override
     public void onTestSkipped(ITestResult iTestResult) {
-
+        System.out.println("Test " + iTestResult.getTestName() + " skipped");
     }
 
     @Override
     public void onTestFailedButWithinSuccessPercentage(ITestResult iTestResult) {
-
+        
     }
 
     @Override
---
+++ src/test/resources/realtor_features/school_rating_and_type.feature
@@ -0,0 +1,3 @@
+Feature: school rating and type test, select schools with entered rating and type
+    Scenario: school rating
+        Given user go to realtor.com home page
