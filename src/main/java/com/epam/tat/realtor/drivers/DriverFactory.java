c81f00d02935278fa7c264d2834a053cc2bd114f
Aliaksei Safronau <Aliaksei_Safronau@epam.com>
2018-10-22 10:02:23 +0300

    del grid

--- src/main/java/com/epam/tat/realtor/drivers/DriverFactory.java
+++ src/main/java/com/epam/tat/realtor/drivers/DriverFactory.java
@@ -3,6 +3,8 @@ package com.epam.tat.realtor.drivers;
 import com.epam.tat.realtor.ConfigProperties;
 import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
@@ -20,14 +22,7 @@ public enum DriverFactory {
         public WebDriver getDriver() {
             if (driver == null) {
                 System.setProperty("webdriver.chrome.driver", ConfigProperties.getTestProperty("chromedriver"));
-                DesiredCapabilities capabilitiesChrome = DesiredCapabilities.chrome();
-                capabilitiesChrome.setPlatform(Platform.WINDOWS);
-
-                try {
-                    driver = new RemoteWebDriver(new URL(ConfigProperties.getTestProperty("remoteURL")), capabilitiesChrome);
-                } catch (MalformedURLException e) {
-                    e.printStackTrace();
-                }
+                driver = new ChromeDriver();
             }
             return driver;
         }
@@ -42,14 +37,7 @@ public enum DriverFactory {
         public WebDriver getDriver() {
             if (driver == null) {
                 System.setProperty("webdriver.gecko.driver", ConfigProperties.getTestProperty("geckodriver"));
-                DesiredCapabilities capabilitiesFirefox = DesiredCapabilities.firefox();
-                capabilitiesFirefox.setPlatform(Platform.WINDOWS);
-
-                try {
-                    driver = new RemoteWebDriver(new URL(ConfigProperties.getTestProperty("remoteURL")), capabilitiesFirefox);
-                } catch (MalformedURLException e) {
-                    e.printStackTrace();
-                }
+                driver = new FirefoxDriver();
             }
             return driver;
         }
