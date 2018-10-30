6896a3c2463b99bd8130d4fdc7c7317fe7f1820a
Anastasiya Khapayeva <Anastasiya_Khapayeva@epam.com>
2018-10-30 13:08:24 +0300

    add home task

--- src/test/java/com/epam/tat/realtor/apitests/HTTPStatusCodeTest.java
+++ src/test/java/com/epam/tat/realtor/apitests/HTTPStatusCodeTest.java
@@ -1,24 +1,26 @@
 package com.epam.tat.realtor.apitests;
 
+import com.epam.tat.realtor.bo.User;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
 import org.testng.Assert;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
-
-import static io.restassured.RestAssured.given;
 
 public class HTTPStatusCodeTest {
 
     @BeforeTest
     public void initPage() {
         RestAssured.baseURI = "https://www.realtor.com/";
+        //RestAssured.basePath = "/sell";
     }
 
     @Test
     public void checkStatusCode() {
-        Response response = given().get("/sell").andReturn();
+        Response response = RestAssured.given().get().andReturn();
         SoftAssert softAssert = new SoftAssert();
         softAssert.assertEquals(response.getStatusCode(), 200);
         softAssert.assertEquals(response.getStatusLine(), "HTTP/1.1 200 OK");
@@ -27,15 +29,18 @@ public class HTTPStatusCodeTest {
 
     @Test
     public void checkHeader(){
-        Response response = given().get("/sell").andReturn();
+        Response response = RestAssured.given().get("/myaccount/my_profile").andReturn();
         SoftAssert softAssert = new SoftAssert();
         softAssert.assertEquals(response.getHeader("content-type"), "application/json");
         softAssert.assertEquals(response.getHeader("charset"), "utf8");
+        softAssert.assertAll();
     }
 
     @Test
-    public void checkResponseBody(){
-
+    public void lastTaskFromHW(){
+        RestTemplate restTeampl = new RestTemplate();
+        ResponseEntity<User[]> response = restTeampl.getForEntity("http://jsonplaceholder.typicode.com/users", User[].class);
+        Assert.assertEquals(response.getBody().length, 10);
     }
 
 }
