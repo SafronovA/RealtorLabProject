4bf02e47a24dd54379157d046d93cb0f380b5fb9
Siarhei Volchak <Siarhei_Volchak@epam.com>
2018-11-12 10:12:17 +0300

    add code

--- src/test/java/com/epam/tat/realtor/mobiletests/APIRequest.java
+++ src/test/java/com/epam/tat/realtor/mobiletests/APIRequest.java
@@ -1,7 +1,40 @@
 package com.epam.tat.realtor.mobiletests;
 
 import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 public class APIRequest {
-    RestAssured restAssured = new RestAssured();
+
+    @BeforeTest
+    public void initPage() {
+        RestAssured.baseURI = "http://mobilecloud.epam.com/automation/api/";
+        //RestAssured.basePath = "/sell";
+    }
+
+    @Test
+    public void checkStatusCode() {
+        Response response = RestAssured.given().header("Authorization","Bearer 815503fd-0af4-420a-ae71-b6a112d82682").get("device/android").andReturn();
+        List<String> deviceList = response.jsonPath().getList("desiredCapabilities.deviceName");
+       // response.prettyPrint();
+        deviceList.stream().forEach(x->System.out.print(x+'\n'));
+    }
+
+    @Test
+    public void checkHeader(){
+        Response response = RestAssured.given().get("/myaccount/my_profile").andReturn();
+        SoftAssert softAssert = new SoftAssert();
+        softAssert.assertEquals(response.getHeader("content-type"), "application/json");
+        softAssert.assertEquals(response.getHeader("charset"), "utf8");
+        softAssert.assertAll();
+    }
+
+
+
 }
